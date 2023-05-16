2a330a20014c609e7434fcdd3e3366efae3d32ce
TrebleBlue <100921695+WampB@users.noreply.github.com>
2023-05-16 16:35:16 +0800

    Update query_filling.py

--- query_filling.py
+++ query_filling.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time,json,random,hold
 
 #we need a special finction to help circulate here
-def opeDiv(i,driver,obj):
+def opeDiv(i):
     lst=list(obj.keys())#获取可以填写的项目
     #step1
     div=driver.find_element(By.ID,"div"+str(i))
@@ -32,21 +32,27 @@ def opeDiv(i,driver,obj):
     else:
         return
 
-def write(link,driver,obj):
+def write():
     #通过问卷url获取并打开
     driver.get(link)
     #try filling the query, if not accessible, fill in by hand
     for i in range(1,13):   #这里实际上可以通过爬虫确定问题数目减少循环次数
         try:
-            opeDiv(i,driver,obj)
+            opeDiv(i)
         except:
             continue
-    nodeOn=driver.find_element(By.XPATH,'//*[@id="ctlNext"]')
-    nodeOn.click()#提交，这一条一般是不变的
-    try:#尝试点击验证，这个不耽误时间，停留0.1秒就够了
+    try:
+        nodeOn=driver.find_element(By.XPATH,'//*[@id="ctlNext"]')
+        nodeOn.click()#提交，这一条一般是不变的
+    except:
+        pass
+    try:    #尝试点击验证，这个不耽误时间，停留0.1秒就够了
         time.sleep(0.5)#必要的间歇时间，没有的话可能找不到节点
-        element = driver.find_element(By.XPATH,'//*[@id="layui-layer1"]/div[3]/a[1]')
-        element.click()#点击确认验证
+        try:
+            element = driver.find_element(By.XPATH,'//*[@id="layui-layer1"]/div[3]/a[1]')
+            element.click()#点击确认验证
+        except:
+            pass
         yanz = driver.find_element(By.XPATH,'//*[@id="SM_BTN_1"]/div[1]/div[4]')
         yanz.click()#点击验证按钮
         print("end time:%d"%time.localtime(time.time())[5])
@@ -62,26 +68,33 @@ def write(link,driver,obj):
 
 #预加载浏览器，加快提交速度
 def openChrome():
-    option = webdriver.ChromeOptions()
-    #可以补充一个后台运行的插件，这样会更快，同样后面需要另一个插件判定是否填写成功
-    #option.add_argument("headless")
-    #加速优化
-    option.add_argument("--disable-images")
-    option.add_argument("--disable-javascript")
-    #优化页面加载策略
-    option.page_load_strategy='eager'   #这一项可能会出现问题，大多数是网络问题，更换网络即可
-    #绕过问卷星的防selenium设置
-    option.add_experimental_option('excludeSwitches', ['enable-automation'])
-    option.add_experimental_option('useAutomationExtension', False)
-    driver = webdriver.Chrome(options=option)
-    driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',{'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
-    return driver
-    
+    d = webdriver.Chrome(options=option)
+    d.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',{'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
+    d.get(link)
+    return d
+
+global driver,obj,link,option
+
+obj=json.load(open("questions.json","r",encoding="utf-8"))  #读取json文件获取要填写的信息
+link=obj["link"]  #嵌入链接
+
+option = webdriver.ChromeOptions()
+#option.add_argument("headless")
+#加速优化
+option.add_argument("--disable-images")
+option.add_argument("--disable-javascript")
+#优化页面加载策略
+option.page_load_strategy='eager'   #这一项可能会出现问题，大多数是网络问题，更换网络即可
+#绕过问卷星的防selenium设置
+option.add_experimental_option('excludeSwitches', ['enable-automation'])
+option.add_experimental_option('useAutomationExtension', False)
+#以安卓系统的微信浏览器为请求头，模拟微信环境[这里解决的是“只能通过微信填写”这个问题]
+option.add_argument('user-agent="Mozilla/5.0 (Linux; U; Android 2.3.6; zh-cn; GT-S5660 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MicroMessenger/4.5.255"')
+
+driver=openChrome() #预加载浏览器
+
 #function main()
 if __name__=="__main__":
-    obj=json.load(open("questions.json","r",encoding="utf-8"))#读取json文件获取要填写的信息
-    driver=openChrome()#预加载浏览器
-    link=input("THE LINK OF THE QUERY:\n")  #嵌入链接
     try:
         hold.hold(18,0,0)
     except:
@@ -89,10 +102,10 @@ if __name__=="__main__":
     print("start time:%d"%time.localtime(time.time())[5])
     while True:
         try:
-            write(link,driver,obj)
+            write()
             break
         except:
             time.sleep(0.2) #这里也可以pass，我主要考虑系统延迟问题需要不断刷新
             continue
-    time.sleep(30)
+    time.sleep(60)
 
